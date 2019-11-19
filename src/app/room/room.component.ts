a4e942ebc21b6a0f91398994ef826cb9e6580609
Zrordran <tibo.mar@orange.fr>
2019-11-19 22:58:03 +0100

    fetchtitre unsubscribe

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NbSidebarService} from '@nebular/theme';
 import {ConfigService} from '../shared/services/config.service';
 import { Video } from '../shared/interfaces/video';
+import {first, sample, take, takeUntil, timeInterval} from 'rxjs/operators';
+import {Subject, timer} from 'rxjs';
 
 
 @Component({
@@ -10,7 +12,8 @@ import { Video } from '../shared/interfaces/video';
   styleUrls: ['./room.component.css'],
   providers: [ConfigService]
 })
-export class RoomComponent implements OnInit {
+export class RoomComponent implements OnInit, OnDestroy {
+  private ngUnsubscribe: any = new Subject();
   videos: { code: string, seen: boolean }[] = [
     { code: 'nxkwg4gNMak', seen: true},
     { code: 's3Q80mk7bxE', seen: false},
@@ -31,13 +34,17 @@ export class RoomComponent implements OnInit {
   }
 
   fetchTitre(code: string): string {
-    this.configService.getData(code).subscribe(
+    this.configService.getData(code).pipe(takeUntil(this.ngUnsubscribe)).subscribe(
       (data: Video) => {
         this.video = {
           items: data
         }, console.log(this.video.items.items[0].snippet.title),
           code = this.video.items.items[0].snippet.title;
-      });
+      }).unsubscribe();
     return code;
+  }
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
